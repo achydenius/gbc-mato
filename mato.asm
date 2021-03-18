86c8a632f5630ca9052864e628ad20985b4b28d0
Arto Chydenius <arto.chydenius@reaktor.com>
2021-03-18 17:09:36 +0200

    Move the snake

--- mato.asm
+++ mato.asm
@@ -6,6 +6,46 @@ JOYPAD_LEFT_BIT     EQU 1
 JOYPAD_UP_BIT       EQU 2
 JOYPAD_DOWN_BIT     EQU 3
 
+BG_TILE_INDEX       EQU 0
+SNAKE_TILE_INDEX    EQU 1
+
+TICK_DIVIDER        EQU $F
+
+; Load a word from memory address
+; \1 = Memory address to the first byte
+; \2 = Register for storing the high byte
+; \3 = Register for storing the low byte
+LoadWord:\
+    MACRO
+    ld a, [\1]
+    ld \2, a
+    ld a, [\1+1]
+    ld \3, a
+    ENDM
+
+; Load a word from hl register, also increment hl
+; \1 = Register for storing the high byte
+; \2 = Register for storing the low byte
+LoadWordHLI:\
+    MACRO
+    ld a, [hl+]
+    ld \1, a
+    ld a, [hl+]
+    ld \2, a
+    ENDM
+
+; Store a word to memory address
+; \1 = High byte
+; \2 = Low byte
+; \3 = Memory address for storing the word
+StoreWord:\
+    MACRO
+    ld a, \1
+    ld [\3], a
+    ld a, \2
+    ld [\3+1], a
+    ENDM
+
 SECTION "Header", ROM0[$100]
     jp Start
 
@@ -13,29 +53,14 @@ SECTION "Code", ROM0[$150]
 Start:
     di                              ; Disable interrupts
 
-    ld hl, Variables                ; Set all variables to zero
-    ld bc, VariablesEnd-Variables
-.clearByte:
-    xor a, a
-    ld [hl+], a
-    dec bc
-    ld a, c
-    or b
-    jr nz, .clearByte
+    StoreWord $0, $4, Length        ; Set length to 4
+    StoreWord $2, $3, Coords        ; Set coordinates to [(3, 2), (3, 1), (2, 1), (1, 1)]
+    StoreWord $1, $3, Coords+2
+    StoreWord $1, $2, Coords+4
+    StoreWord $1, $1, Coords+6
 
-    ld a, $0                        ; Set up test data for snake
-    ld [Length], a
-    ld a, $3
-    ld [Length+1], a
-    ld hl, TmpData
-    ld bc, Coords
-    ld d, $6
-.copyCoord:
-    ld a, [hl+]
-    ld [bc], a
-    inc bc
-    dec d
-    jr nz, .copyCoord
+    ld a, $0                        ; Zero ticks
+    ld [Ticks], a
 
     call WaitVBlank                 ; Turn off LCD
     ld hl, rLCDC
@@ -64,49 +89,89 @@ Start:
     ld hl, rLCDC                    ; Turn on LCD
     set LCDCF_ON_BIT, [hl]
 
+    ; Draw initial state
+    call WaitVBlank
+
+    LoadWord Length, d, e           ; de = Loop counter
+    ld hl, Coords
+.drawCoord:
+    LoadWordHLI b, c
+
+    push hl
+    call GetTileAddress
+    ld a, SNAKE_TILE_INDEX
+    ld [hl], a
+    pop hl
+
+    dec de                          ; Loop
+    ld a, d
+    or e
+    jr nz, .drawCoord
+
+    ; Main loop where snake is moving right
 Loop:
     call WaitVBlank
 
-    call ReadArrowKeys              ; Read arrow keys to a
-    bit JOYPAD_RIGHT_BIT, a
-    jr z, .testDown
-.testDown:
-    bit JOYPAD_DOWN_BIT, a
-    jr z, .testLeft
-.testLeft:
-    bit JOYPAD_LEFT_BIT, a
-    jr z, .endTest
+    ld hl, Ticks
+    inc [hl]
 
-.endTest:
-    ld a, [Length]
-    ld d, a
-    ld a, [Length+1]
-    ld e, a
+    ld a, [Ticks]                   ; Update snake every nth frame
+    and a, TICK_DIVIDER
+    jr nz, Loop
 
-    ld hl, Coords
-.drawCoord:
-    ld a, [hl+]
-    ld b, a
-    ld a, [hl+]
-    ld c, $1
-
-    call SetTile
-
+    ; Update coordinates
+    LoadWord Length, d, e           ; de = Loop counter (Length-1)
     dec de
-    ld a, e
-    or d
-    jr nz, .drawCoord
+    ld hl, Coords                   ; Get address to the last coordinate pair
+    add hl, de
+    add hl, de
+
+    ; Clear last coordinate
+    push hl
+    LoadWordHLI b, c
+    call GetTileAddress
+    ld a, BG_TILE_INDEX
+    ld [hl], a
+    pop hl
+
+    dec hl                          ; hl = Address to second last x-coordinate
+.update:
+    ld b, h                         ; bc = Address to next x-coordinate
+    ld c, l
+    inc bc
+    inc bc
+
+    ld a, [hl-]                     ; Replace next coordinate pair with current one
+    ld [bc], a
+    dec bc
+    ld a, [hl-]
+    ld [bc], a
+
+    dec de                          ; Loop
+    ld a, d
+    or e
+    jr nz, .update
+
+    ; Update head
+    ; TODO: Replace with current direction
+    inc hl
+    inc hl
+    inc [hl]
+
+    ; Draw updated first coordinate
+    LoadWord Coords, b, c
+    call GetTileAddress
+    ld a, SNAKE_TILE_INDEX
+    ld [hl], a
 
     jr Loop
 
-; Set tile in tilemap
-; a = x-coordinate
+; Get address of a tile in tilemap
 ; b = y-coordinate
-; c = Tile number
-; Overwrites a
-SetTile:
-    push hl
-    push de
+; c = x-coordinate
+; Returns the address in hl
+GetTileAddress:
+    push bc
 
     ld h, $0
     ld l, b
@@ -114,17 +179,14 @@ SetTile:
     add hl, hl
     ENDR
 
-    ld d, $0                        ; Add x coordinate
-    ld e, a
-    add hl, de
+    ld b, $0                        ; Add x coordinate
+    add hl, bc
 
-    ld de, _SCRN0                   ; Add tilemap base address
-    add hl, de
-    ld a, c                         ; Set the tile
-    ld [hl], a
+    ld bc, _SCRN0                   ; Add tilemap base address
+    add hl, bc
 
-    pop de
-    pop hl
+    pop bc
+    ret
 
 ; Return arrow keys state in register a
 ReadArrowKeys:
@@ -151,4 +213,5 @@ SECTION "Variables", WRAM0[$C000]
 Variables:
 Length: DS 2
 Coords: DS 20 * 18 * 2
+Ticks:  DS 1
 VariablesEnd:
